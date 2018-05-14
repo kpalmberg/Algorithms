93cb9690a493dd0186afd5039bc217f4589925af
Kpalmberg <palmberg535@gmail.com>
2018-05-14 01:35:51 -0700

    Swapped over to C from C++

---
+++ merge_sort/merge_sort.h
@@ -0,0 +1,11 @@
+#ifndef INSERTION_H
+#define INSERTION_H
+
+#include <stdio.h>
+
+void merge(int A[], int left, int middle, int right);
+void mergeSort(int A[], int left, int right);
+void doMergeSort(int A[], int length);
+void printArray(int A[], int length);
+
+#endif
